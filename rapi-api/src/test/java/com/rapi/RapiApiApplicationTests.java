04d61dd0fb05d741586fe11c1d4f03336ce0146e
nicoerlando <nicolas.erlando@gmail.com>
2026-09-05 22:28:17 +0800

    Initialized Project with SpringBootApplication

---
+++ rapi-api/src/test/java/com/rapi/RapiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
